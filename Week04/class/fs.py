596df5386b012212561be148574b7eb5caad42ba
smeredith116 <smeredith116@gmail.com>
2022-02-10 15:13:53 -0500

    Edits

---
+++ Week04/class/fs.py
@@ -0,0 +1,13 @@
+# File to traverse a given directory andf it's subdirs and retrieve all the files
+
+import os, sys
+
+# Get infomation from the commandline
+#print(sys,argv)
+#Directory to traverse
+rootdir = sys.argv[1]
+#print(rootdir)
+
+# In our story, we will traverse a directory
+
+# Check if this argument
