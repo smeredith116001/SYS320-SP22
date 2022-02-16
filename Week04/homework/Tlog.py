b161efacb5939a8b1eb387e7551ffd7fbec81e73
smeredith116 <smeredith116@gmail.com>
2022-02-15 19:58:33 -0500

    Homework Week 4

---
+++ Week04/homework/Tlog.py
@@ -0,0 +1,47 @@
+#Credit to Duane Dunston
+import logCheck
+import importlib
+importlib.reload(logCheck)
+
+
+def proxy_events(filename, service, term):
+    
+    is_found = logCheck._logs(filename,service,term)
+    # Found List
+    found = []
+    #Looping through
+    for eachFound in is_found:
+        #Split the results
+        sp_results = eachFound.split(" ")
+        # Appending the results
+        #GET /cgi-bin/welcome HTTP/1.1" 404 435
+        found.append(sp_results[1] + " " + sp_results[3] + " " + sp_results[4])
+        
+    # Print  
+    getValues = set(found)
+    
+    for eachValue in getValues:
+        
+        print(eachValue)
+        
+    
+
+   
+    
+    
+                    
+                     
+                     
+        
+        
+   
+
+ 
+        
+
+        
+
+                                   
+
+    
+    
