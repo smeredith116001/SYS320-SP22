94fd6de8e6dce6dc500f6c3d74bca6b5d1c976d6
smeredith116 <smeredith116@gmail.com>
2022-04-26 21:27:25 -0400

    Homework Week 13 Commit

--- Week12/homework/Update.bat
+++ Week12/homework/Update.bat
@@ -1,2 +1,17 @@
 del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
 del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
+del C:\Users\Sebby\SYS320-SP22-New\SYS320-SP22\Week12\Homework\step2test.ps1
